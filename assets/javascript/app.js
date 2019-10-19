76cc064632dcf0e427d7ddb6d78d6c4799950b82
humberto24 <humberto24@gmail.com>
2019-10-18 22:35:17 -0500

    This commit contains the latest version of the TriviaGame code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,80 @@
 $(function(){
    
-    // Variables setup
-    var triviaPart = $("#trivia-content-div");
-    var allDonePart = $("#all-done");
-    var startBtn = $("#start-button");
-    var startDiv = $("#start-div");
+    // The general variables ar set up ahead:
 
-    // Trivia Questions Part
+        // Get elements from DOM
+        var triviaPart = $("#trivia-content-div");
+        var allDonePart = $("#all-done");
+        var startDiv = $("#start-div");
+
+        // Buttons
+        var startBtn = $("#start-button");
+        var finishedBtn = $("#finished-button");
+        var tryAgainBtn = $("#try-again-button");
+        var inputs = $(".input-r");
+
+        // Radio Inputs
+        var radInput = $(inputs).attr("value");
+
+
+        // Correct vs Incorrect counters
+        var correct = 0;
+        var incorrect = 0;
+        var unaswered = 0;
+
+
+    // This code get the current year for the footer copyright part:
+    var today = new Date();
+    var year = today.getFullYear();
+    $("#current-year").text(year);
+
+
+    // This happens when the page loads:
     $(triviaPart).hide();
     $(allDonePart).hide();
 
 
-    // Show trivia questions
+    // These methods setup the timer:
+
+
+
+    // This happens when the "Start" button is clicked on:
     $(startBtn).on("click", function(){
         $(startDiv).hide();
         $(triviaPart).show();
     });
     
-    // Set timer
+
+    // This happens when the "Finised" button is clicked:
+    $(finishedBtn).on("click", function(){
+        $(triviaPart).hide();
+        $(allDonePart).show();
+
+        var checked = $(".input").checked;
+        console.log("Checked: "+ checked)
+
+
+        // // These conditionals add to the correct vs incorrect counter variables:
+        // $(".input-r").each(function(){
+        //     if($(".input-r").attr("value","correct").checked){
+        //         correct++
+        //         console.log("Correct value: " + correct);
+        //         console.log("RadInput: " + radInput);
+        //     };
+        // });
+        
+        
+
+    });
+
+    // This happens when the "Try Again" button is clicked on:
+    $(tryAgainBtn).on("click",function(){
+        $(triviaPart).hide();
+        $(allDonePart).hide();
+        $(startDiv).show();
+    });
+
+    
 
 
 
